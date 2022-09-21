2ad0b6fb8211cd0f6ad1c9f945e6a82ef2b0d311
Yekteniya2202 <yellowblue924@yandex.ru>
2022-09-21 16:30:46 +0300

    lab 1 modified

--- CryptoLab1/src/Main.java
+++ CryptoLab1/src/Main.java
@@ -8,39 +8,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Character> alphabet = getRussianAlphabet();
-        String[] cesarKey = readFile("src/afinCesarKey.txt").split(" ");
-        String[] magicKey = readFile("src/magicSquareKey.txt").split(" ");
-        String toEncrypt = readFile("src/encrypt.txt");
+        try {
+            String[] cesarKey = readFile("src/afinCesarKey.txt").split(" ");
+            String[] magicKey = readFile("src/magicSquareKey.txt").split(" ");
+            String toEncrypt = readFile("src/encrypt.txt");
 
-        if (cesarKey.length != 2){
-            throw new RuntimeException("cesar key not valid");
+            if (cesarKey.length != 2) {
+                throw new RuntimeException("cesar key not valid");
+            }
+            System.out.println(Arrays.toString(readFile("src/magicSquareKey.txt").split(" ")));
+            AfinCesarEncryptor afinCesarEncryptor = new AfinCesarEncryptor(alphabet, Integer.parseInt(cesarKey[0]), Integer.parseInt(cesarKey[1]));
+            int n = Integer.parseInt(magicKey[0]);
+            ArrayList<Integer> key = new ArrayList<>();
+            if (n * n != magicKey.length - 1) {
+                throw new RuntimeException("magic key not valid");
+            }
+            for (int i = 1; i < magicKey.length; i++) {
+                key.add(Integer.parseInt(magicKey[i]));
+            }
+            MagicSquareEncryptor magicSquareEncryptor = new MagicSquareEncryptor(n, key);
+
+            System.out.println("Encypting");
+            String afinEncrypted = afinCesarEncryptor.encrypt(toEncrypt);
+            System.out.println("Afin encrypted to \"" + afinEncrypted + "\" (chech to decrypt - \"" + afinCesarEncryptor.decrypt(afinEncrypted) + "\")");
+            String magicSquareEncrypted = magicSquareEncryptor.encrypt(afinEncrypted);
+            System.out.println("Magic square encrypted to \"" + magicSquareEncrypted + "\" (chech to decrypt - \"" + magicSquareEncryptor.decrypt(magicSquareEncrypted) + "\")");
+            System.out.println("=======================");
+            System.out.println("Decrypting");
+            String magicSquareDecrypted = magicSquareEncryptor.decrypt(magicSquareEncrypted);
+            System.out.println("Magic square decrypted to \"" + magicSquareDecrypted + "\" (chech to encrypt - \"" + magicSquareEncryptor.encrypt(magicSquareDecrypted) + "\")");
+            String afinDecrypted = afinCesarEncryptor.decrypt(magicSquareDecrypted);
+            System.out.println("Afin decrypted to \"" + afinDecrypted + "\" (chech to encrypt - \"" + afinCesarEncryptor.encrypt(afinDecrypted) + "\")");
+
+            writeFile("src/decrypt.txt", afinDecrypted);
         }
-        System.out.println(Arrays.toString(readFile("src/magicSquareKey.txt").split(" ")));
-        AfinCesarEncryptor afinCesarEncryptor = new AfinCesarEncryptor(alphabet, Integer.parseInt(cesarKey[0]), Integer.parseInt(cesarKey[1]));
-        int n = Integer.parseInt(magicKey[0]);
-        ArrayList<Integer> key = new ArrayList<>();
-        if (n*n != magicKey.length - 1){
-            throw new RuntimeException("magic key not valid");
+        catch (Exception e){
+            System.out.println(e.getMessage());
         }
-        for(int i = 1; i < magicKey.length; i++){
-            key.add(Integer.parseInt(magicKey[i]));
-        }
-        MagicSquareEncryptor magicSquareEncryptor = new MagicSquareEncryptor(n, key);
-
-        System.out.println("Encypting");
-        String afinEncrypted = afinCesarEncryptor.encrypt(toEncrypt);
-        System.out.println("Afin encrypted to \"" + afinEncrypted + "\" (chech to decrypt - \"" + afinCesarEncryptor.decrypt(afinEncrypted) + "\")");
-        String magicSquareEncrypted = magicSquareEncryptor.encrypt(afinEncrypted);
-        System.out.println("Magic square encrypted to \"" + magicSquareEncrypted + "\" (chech to decrypt - \"" + magicSquareEncryptor.decrypt(magicSquareEncrypted) + "\")");
-        System.out.println("=======================");
-        System.out.println("Decrypting");
-        String magicSquareDecrypted = magicSquareEncryptor.decrypt(magicSquareEncrypted);
-        System.out.println("Magic square decrypted to \"" + magicSquareDecrypted + "\" (chech to encrypt - \"" + magicSquareEncryptor.encrypt(magicSquareDecrypted) + "\")");
-        String afinDecrypted = afinCesarEncryptor.decrypt(magicSquareDecrypted);
-        System.out.println("Afin decrypted to \"" + afinDecrypted + "\" (chech to encrypt - \"" + afinCesarEncryptor.encrypt(afinDecrypted) + "\")");
-
-        writeFile("src/decrypt.txt", afinDecrypted);
-
     }
 
     private static String readFile(String path) {
@@ -67,12 +71,13 @@ public class Main {
 
     private static ArrayList<Character> getRussianAlphabet() {
         ArrayList<Character> alphabet = new ArrayList<>();
-        for (char c = 'А'; c <= 'я'; c++) {
+        for (char c = 'А'; c <= 'Я'; c++) {
             alphabet.add(c);
         }
         alphabet.add(' ');
         alphabet.add(',');
         alphabet.add('.');
+        alphabet.add('Ё');
         return alphabet;
     }
 }
